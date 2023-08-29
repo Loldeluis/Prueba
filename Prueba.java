9db0d0482664b987295340f737a3bb923b060944
Loldeluis <luismaldonadourib@gmail.com>
2023-08-28 20:26:50 -0500

    Updated

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Prueba{
 public static void main(String[] args) {
-    System.out.println("Hola mundo");
+    Scanner leer = new Scanner(System.in);
+
+
+    System.out.println("Digite dos numeros");
+    int a = leer.nextInt();
+    int b = leer.nextInt();
+
 }
 
 }
